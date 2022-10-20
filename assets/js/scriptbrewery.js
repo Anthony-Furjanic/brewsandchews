0c2d4c9c8974a7a20abc9bf73f1668d7203ed5cc
Amy Lipscomb <amylipscomb@amys-mbp.myfiosgateway.com>
2022-10-19 21:09:36 -0400

    created a div within index.html to create the id- 'citySearch'; then created a new element (h3)  within the scriptbrewery.html, and then modified the element to include the phone number, and address

--- assets/js/scriptbrewery.js
+++ assets/js/scriptbrewery.js
@@ -1,83 +1,11 @@
-// var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
-// var badRequestUrl = 'https://api.github.com/orgs/nodejddd/repad';
-
-// fetch(requestUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
-
-// fetch(badRequestUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'f18ada2c48msh78033a43062fe9fp17cd5cjsnd0092509d56d',
-// 		'X-RapidAPI-Host': 'brianiswu-open-brewery-db-v1.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/5494', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'f18ada2c48msh78033a43062fe9fp17cd5cjsnd0092509d56d',
-// 		'X-RapidAPI-Host': 'brianiswu-open-brewery-db-v1.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=dog', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-// var fetchButton = document.getElementById('fetch-button');
-
-// // Get Breweries 
-
-// function getApi() {
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'f18ada2c48msh78033a43062fe9fp17cd5cjsnd0092509d56d',
-// 		'X-RapidAPI-Host': 'brianiswu-open-brewery-db-v1.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?by_state=NY&by_name=cooper&by_type=micro&by_tag=patio', options)
-// 	.then(response => response.json())
-// 	.then(response => {
-//     console.log(response);
-//     console.log(response.content);
-//   })
-// 	.catch(err => console.error(err));
-// }
 
 var fetchButton = document.getElementById('fetch-button');
 fetchButton.addEventListener('click', getApi);
+var citySearch = document.getElementById('citySearch');
 
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'f18ada2c48msh78033a43062fe9fp17cd5cjsnd0092509d56d',
-// 		'X-RapidAPI-Host': 'brianiswu-open-brewery-db-v1.p.rapidapi.com'
-// 	}
-// };
 function getApi(){
-fetch("https://api.openbrewerydb.org/breweries?by_city=san_diego&per_page=3")
+fetch(`https://api.openbrewerydb.org/breweries?by_city=${citySearch.value}&per_page=3`)
 	.then(response => response.json())
   .then(function(data){
     console.log(data)
@@ -86,21 +14,24 @@ fetch("https://api.openbrewerydb.org/breweries?by_city=san_diego&per_page=3")
       var barName = data[i].name 
       console.log(barName)
 
-      // create
+// create
 var $li = $("<li>");
 var $h1 = $("<h1>");
 var $h2 = $("<h2>");
+var $h3 = $("<h3>");
 
-
-      // modify
+// modify
 $h1.text(barName);
+$h2.text(data[i].phone);
+$h3.text(data[i].street+" "+data[i].state+" "+data[i].postal_code);
+
 // to do location!
 
 
 
-      // append
+// append
 $("#breweries-results ul").append($li)
-$li.append($h1, $h2)
+$li.append($h1, $h2, $h3)
 
     }
   })
