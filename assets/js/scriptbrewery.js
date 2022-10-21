592e721f5a2251349018f8186ec6a9425e49f02e
Cassandre0617 <>
2022-10-20 21:05:28 -0400

    updated js

--- assets/js/scriptbrewery.js
+++ assets/js/scriptbrewery.js
@@ -1,9 +1,12 @@
 
+
 var fetchButton = document.getElementById('fetch-button');
+// button handler to search for breweries
 fetchButton.addEventListener('click', getApi);
+
 var citySearch = document.getElementById('citySearch');
 
-
+// call API
 function getApi(){
 fetch(`https://api.openbrewerydb.org/breweries?by_city=${citySearch.value}&per_page=3`)
 	.then(response => response.json())
@@ -11,30 +14,39 @@ fetch(`https://api.openbrewerydb.org/breweries?by_city=${citySearch.value}&per_p
     console.log(data)
     for(var i=0; i < data.length; i++){
 
-      var barName = data[i].name 
-      console.log(barName)
+// variable to get brewery name
+      var breweryName = data[i].name 
+      console.log(breweryName)
       displayBeerInfo(data[i]); // AL Update- Recently added this to the for-loop, since I created a function for it below.
 
-// create
+// create tags for each object
 var $li = $("<li>");
 var $h1 = $("<h1>");
 var $h2 = $("<h2>");
 var $h3 = $("<h3>");
+var $h4 = $("<h4>");
 
-// modify
-$h1.text(barName);
+// modify tags to contain brewery data 
+$h1.text(breweryName);
 $h2.text(data[i].phone);
 $h3.text(data[i].street+" "+data[i].state+" "+data[i].postal_code);
+$h4.text(data[i].website_url);
 
-// append
+// append the elements
 $("#breweries-results ul").append($li)
-$li.append($h1, $h2, $h3)
+$li.append($h1, $h2, $h3, $h4)
 
     }
   })
 
 	.catch(err => console.error(err));
 }
+
+function searchNewCity(){
+  document.getElementById("reset").reset();
+}
+// getApi.clear();
+getApi();
 
 //AL UPDATE-
 
