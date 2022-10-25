1603916c6b498ea1adcc83163c87ca012a30f09e
unknown <anthonyf427@hotmail.com>
2022-10-24 20:43:30 -0400

    bob assisted with randomizing the answer set

--- assets/js/Triviacode.js
+++ assets/js/Triviacode.js
@@ -87,10 +87,30 @@ function QuestionRetrieve() {
   console.log(randomQuestion.answers);
 
   document.getElementById("questionField").innerHTML = randomQuestion.questions;
-  document.getElementById("answer1").innerHTML = randomQuestion.answers[0];
-  document.getElementById("answer2").innerHTML = randomQuestion.answers[1];
-  document.getElementById("answer3").innerHTML = randomQuestion.rightans[0];
+  const answerelement= ["answer1","answer2","answer3"];
+  for (var i=0; i< 4;i++)
+  {
+    var num1= Math.floor(Math.random() * answerelement.length);
+    var num2= Math.floor(Math.random() * answerelement.length);
+    if (num1 != num2){
+      var temp= answerelement[num1];
+      answerelement[num1]= answerelement[num2];
+      answerelement[num2]= temp
+    }
+  }
+  document.getElementById(answerelement[0]).innerHTML = randomQuestion.answers[0];
+  document.getElementById(answerelement[1]).innerHTML = randomQuestion.answers[1];
+  document.getElementById(answerelement[2]).innerHTML = randomQuestion.rightans[0];
 
+  document.getElementById(answerelement[0]+"input").setAttribute("data-correct","wrong")
+  document.getElementById(answerelement[1]+"input").setAttribute("data-correct","wrong") 
+  document.getElementById(answerelement[2]+"input").setAttribute("data-correct","correct")
+
+function checkanswer(){
+  checkanswer=document.querySelector("[name=groupOfAnswers]:checked").getAttribute("data-correct")
+}
+TriviaButton2.addEventListener("click",checkanswer)
+  
   //   trivquestions = trivquestions[Math.floor(Math.random()*
   //     (trivquestions.length))];
   //     return trivquestions
