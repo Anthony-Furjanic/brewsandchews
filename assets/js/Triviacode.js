cfffd35a7db0498102d5ec5df6111f3e8b6b71a5
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2022-10-24 22:30:49 -0400

    commit

--- assets/js/Triviacode.js
+++ assets/js/Triviacode.js
@@ -28,55 +28,55 @@ const questionAndAnswer = [
   {
     questions: "What is the name of the process of making Beer?",
     answers: ["Beering", "Alchemy"],
-    rightans: ["Brewing"],
+    rightAns: ["Brewing"],
   },
   {
     questions:
       "Humans all over the world consume approximately how much beer in a year?",
     answers: ["337 Million Gallons", "8 billion gallons"],
-    rightans: ["50 billion gallons"],
+    rightAns: ["50 billion gallons"],
   },
   {
     questions:
       "The building that is dedicated to making beer is known as what?",
     answers: ["Brewhouse", "Beergarden"],
-    rightans: ["Brewery"],
+    rightAns: ["Brewery"],
   },
   {
     questions: "According to botany the study of beer is known as what?",
     answers: ["Hopology", "fermentology"],
-    rightans: ["Zythology"],
+    rightAns: ["Zythology"],
   },
   {
     questions: "What is the most famous style of beer?",
     answers: ["Lager", "Stout"],
-    rightans: ["Pilsner"],
+    rightAns: ["Pilsner"],
   },
   {
     questions: "Which country leads the most consumption of beer per capita?",
     answers: ["Germany", "United States"],
-    rightans: ["Czech Republic"],
+    rightAns: ["Czech Republic"],
   },
   {
     questions: "International beer day is celebrated when?",
     answers: ["3rd Day of Septmember", "17th Day of April"],
-    rightans: ["1st Day of August"],
+    rightAns: ["1st Day of August"],
   },
   {
     questions:
       "After beer what is the second most consumed drink on the earth (excluding water)?",
     answers: ["Lemonade", "Soda"],
-    rightans: ["Tea"],
+    rightAns: ["Tea"],
   },
   {
     questions: "What is the oldest beverage in the world?",
     answers: ["Fermented fruit juice", "vodka"],
-    rightans: ["Mead (An alcoholic drink that was discovered mistakenly)"],
+    rightAns: ["Mead (An alcoholic drink that was discovered mistakenly)"],
   },
   {
     questions: "Which country has the most individual brands of Beer?",
     answers: ["France", "Canada"],
-    rightans: ["Belgium"],
+    rightAns: ["Belgium"],
   },
 ];
 
@@ -89,7 +89,7 @@ function QuestionRetrieve() {
   document.getElementById("questionField").innerHTML = randomQuestion.questions;
   document.getElementById("answer1").innerHTML = randomQuestion.answers[0];
   document.getElementById("answer2").innerHTML = randomQuestion.answers[1];
-  document.getElementById("answer3").innerHTML = randomQuestion.rightans[0];
+  document.getElementById("answer3").innerHTML = randomQuestion.rightAns[0];
 
   //   trivquestions = trivquestions[Math.floor(Math.random()*
   //     (trivquestions.length))];
